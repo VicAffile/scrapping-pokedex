d934acbf12630bea74fc9f824101d3ea070e69ef
VicAffile <victoraffile@gmail.com>
2021-12-09 22:35:02 +0100

    Utilisation d'un seul nom de pokémon converti pour les url de Poképédia et Pokébip

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import unidecode as unidecode
 from selenium import webdriver
 
 from classes.pokemon import Pokemon
 
+nom_pokepedia = "Salamèche"
+nom_pokebip = unidecode.unidecode(nom_pokepedia.lower())
+
 pokepedia = webdriver.Chrome(executable_path="chromedriver.exe")
-pokepedia.get("https://www.pokepedia.fr/Chenipotte")
+pokepedia.get("https://www.pokepedia.fr/" + nom_pokepedia)
 pokebip = webdriver.Chrome(executable_path="chromedriver.exe")
-pokebip.get("https://www.pokebip.com/pokedex/pokemon/chenipotte")
+pokebip.get("https://www.pokebip.com/pokedex/pokemon/" + nom_pokebip)
 
 pokemon = Pokemon(pokepedia, pokebip)
 pokemon.afficher()
