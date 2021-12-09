d200f9210cb4161d3dd81dba65af91a2256fb954
VicAffile <victoraffile@gmail.com>
2021-12-09 23:19:21 +0100

    Scrap de la liste des Pokémons

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import os
 import unidecode as unidecode
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from classes.pokemon import Pokemon
 
+
+annuaire = webdriver.Chrome(executable_path="chromedriver.exe")
+annuaire.get("https://www.pokepedia.fr/Liste_des_Pok%C3%A9mon_dans_l%27ordre_du_Pok%C3%A9dex_National")
+
+liste_pokemon = []
+pokemons = annuaire.find_elements(By.XPATH, "//table[@class='tableaustandard sortable entetefixe jquery-tablesorter' or @class='tableaustandard centre sortable entetefixe jquery-tablesorter']//tbody//tr//td[3]")
+for pokemon in pokemons:
+    region = str(pokemon.text).split(" ")[0]
+    if region != "Alolan" and region != "Galarian" and region != "Hisuian":
+        liste_pokemon.append(pokemon.text)
 
 for filename in os.listdir("images/sprites"):
     os.remove("images/sprites/" + filename)
